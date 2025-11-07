189dfdab1f0a3ac3cca8c1d387726ce99220fba1
kmn1625 <101081743+kmn1625@users.noreply.github.com>
2025-11-07 15:06:08 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -121,15 +121,3 @@ resource "aws_instance" "vm" {
   tags = merge({ Name = "jenkins-tf-ubuntu-ec2" }, var.tags)
 }
 
-output "instance_id" {
-  value = aws_instance.vm.id
-}
-
-output "public_ip" {
-  value = aws_instance.vm.public_ip
-}
-
-output "ssh_command" {
-  value       = "ssh -i ${path.module}/jenkins-ec2.pem ubuntu@${aws_instance.vm.public_ip}"
-  description = "SSH command (download PEM from Jenkins artifacts first)"
-}
